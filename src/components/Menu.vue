7f5058170f10b382f7822c9158d54a9d763aeba2
Ryan <ryangleason82@gmail.com>
2020-07-26 09:37:27 -0400

    First commit

---
+++ src/components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h1 class="mt-4 text-center">Menu</h1>
+    <div v-for="menu in menuData" :key="menu.title">
+      <h4 class="mt-4 text-left">{{menu.category}}</h4>
+      <hr />
+      <div v-for="item in menu.items" :key="item.title">
+        <h5 class="mt-4">{{item.title}}</h5>
+        <p>{{item.description}}</p>
+      </div>
+    </div>
+  </div>
+
+  
+</template>
+
+<script>
+const menuList = require('../data/Menu.json')
+export default {
+  name: 'Menu',
+  data(){
+    return {
+      menuData: []
+    }
+  },
+  mounted(){
+    this.menuData = menuList
+  }
+}
+</script>
+
+<style scoped>
+</style>
